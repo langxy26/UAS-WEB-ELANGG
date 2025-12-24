1223b2f7b6a385e0cacefc06e1444c54c1165587
langxy26 <mhmmdgilang472@gmail.com>
2025-12-24 23:14:50 +0700

    hehe

--- script.js
+++ script.js
@@ -3,19 +3,3 @@ function sendAlert(event) {
     alert("Terima kasih! Pesan kamu sudah terkirim 😊");
     event.target.reset();
 }
-
-/* DARK LIGHT MODE */
-const toggleBtn = document.getElementById("themeToggle");
-const icon = toggleBtn.querySelector("i");
-
-toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("light-mode");
-
-    if (document.body.classList.contains("light-mode")) {
-        icon.classList.remove("fa-sun");
-        icon.classList.add("fa-moon");
-    } else {
-        icon.classList.remove("fa-moon");
-        icon.classList.add("fa-sun");
-    }
-});
